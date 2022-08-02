62578bb5a57561831aece4df481493770e20582c
thiagosougo1 <thiagosg@ciandt.com>
2022-08-02 14:37:42 -0300

    Ex 0

--- 0x01/spring_boot_1/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/com/example/demo/MessageResource.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,15 @@ public class MessageResource {
     @GetMapping("/simpleMessageWelcome")
     public String simpleMessageWelcome() {
         return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+    }
+
+    @GetMapping("/login/{user}/{password}")
+    public String login(@PathVariable String user, @PathVariable String password) {
+        if(user == "" || password == ""){
+            return "USUÁRIO E SENHA NÃO INFORMADOS";
+        } else if(user.length() > 15 || password.length() > 15){
+            return "USUÁRIO E SENHA NÃO INVÁLIDOS";
+        }
+        return "LOGIN EFETUADO COM SUCESSO !!!";
     }
 }
