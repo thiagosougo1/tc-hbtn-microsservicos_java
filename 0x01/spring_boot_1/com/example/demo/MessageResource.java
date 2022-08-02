107c3338314a54c55165cc3558d1dc245791f3bb
thiagosougo1 <thiagosg@ciandt.com>
2022-08-02 14:39:10 -0300

    Ex 0

--- 0x01/spring_boot_1/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/com/example/demo/MessageResource.java
@@ -18,7 +18,7 @@ public class MessageResource {
         if(user == "" || password == ""){
             return "USUÁRIO E SENHA NÃO INFORMADOS";
         } else if(user.length() > 15 || password.length() > 15){
-            return "USUÁRIO E SENHA NÃO INVÁLIDOS";
+            return "USUÁRIO E SENHA INVÁLIDOS";
         }
         return "LOGIN EFETUADO COM SUCESSO !!!";
     }
