fec86ac76a759619f4e4a615b9c61ad44265d972
thiagosougo1 <thiagosg@ciandt.com>
2022-08-10 22:06:11 -0300

    Ex 0

--- 0x07/api-rest-calculator/src/main/java/com/example/calculator/model/CalculatorTest.java
+++ 0x07/api-rest-calculator/src/main/java/com/example/calculator/model/CalculatorTest.java
@@ -1,60 +1,51 @@
 package com.example.calculator.model;
 
-import java.time.LocalDate;
-import java.util.*;
+import org.testng.annotations.Test;
 
 public class CalculatorTest {
 
-    public Double sum(Double number1, Double number2) {
+    @org.junit.Test
+    void sumTest() {
         // TODO
-        // validação -> throw new NullPointerException("Número 1 e número 2 são obrigatórios.");
-        return null;
     }
 
-    public Double sub(Double number1, Double number2) {
+    @Test
+    public void numbersNullSumTest() {
         // TODO
-        // validação -> throw new NullPointerException("Número 1 e número 2 são obrigatórios.");
-        return null;
     }
 
-    public Double divide (Double number1, Double number2)  {
+    @Test
+    void subTest() {
         // TODO
-        // validação -> throw new NullPointerException("Número 1 e número 2 são obrigatórios.");
-        // validação -> throw new ArithmeticException("Divisão por zero não é permitido.");
-        return null;
     }
 
-    public Integer factorial(Integer factorial) {
+    @Test
+    void divideTest() {
         // TODO
-        // validação -> throw new NullPointerException("Número é obrigatório.");
-        return null;
     }
 
-    // Exemplos:
-    // Integer = 1 -> Binary = 1
-    // Integer = 5 -> Binary = 101
-    // Integer = 20 -> Binary = 10100
-    public Integer integerToBinary(Integer integer) {
+    @Test
+    public void divisionByZeroTest() {
         // TODO
-        return null;
     }
 
-    // Exemplos:
-    // Integer = 1 -> Hexadecimal = "1"
-    // Integer = 5 -> Hexadecimal = "37"
-    // Integer = 170 -> Binary = "AA"
-    public String integerToHexadecimal(Integer integer) {
+    @Test
+    void factorialTest() {
         // TODO
-        return null;
     }
 
-    // Exemplos
-    // Date 1 = LocalDate.of(2020, 3, 15);
-    // Date 2 = LocalDate.of(2020, 3, 29)
-    // Total de dias = 14
-    public int calculeDayBetweenDate(LocalDate date1, LocalDate date2) {
+    @Test
+    void integerToBinaryTest() {
         // TODO
-        return 0;
     }
 
+    @Test
+    void integerToHexadecimalTest() {
+        // TODO
+    }
+
+    @Test
+    void calculeDayBetweenDateTest() {
+        // TODO
+    }
 }
