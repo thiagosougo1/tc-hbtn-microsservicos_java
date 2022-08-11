557601d1685ed466dd40fcbd55c331c022fcc7ee
thiagosougo1 <thiagosg@ciandt.com>
2022-08-10 21:46:05 -0300

    Ex 0

---
+++ 0x06/api-rest-userController/src/main/java/com/example/user/exception/ExceptionService.java
@@ -0,0 +1,17 @@
+package com.example.user.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class ExceptionService {
+
+    @ExceptionHandler
+    ResponseEntity<CPFException> handleException(CPFException err){
+        UserErrorResponse uer =new UserErrorResponse();
+        uer.setStatus(HttpStatus.NOT_FOUND.value());
+        uer.setMessage("You have entered CPF "+err.getMessage()+" invalid.");
+        return new ResponseEntity(uer,HttpStatus.NOT_FOUND);
+    }
+}
